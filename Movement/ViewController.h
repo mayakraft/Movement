27bc080a739bbc5dc98a31f3e7b63cf7e6674aa8
robbykraft <robbykraft@gmail.com>
2013-11-01 19:52:14 -0700

    Initial Commit

---
+++ Movement/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Movement
+//
+//  Created by Robby Kraft on 11/1/13.
+//  Copyright (c) 2013 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
---
+++ Movement/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Movement
+//
+//  Created by Robby Kraft on 11/1/13.
+//  Copyright (c) 2013 Robby Kraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
